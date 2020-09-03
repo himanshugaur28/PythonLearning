3d86d7ce4c7b38577b8d9191d243a6fdbf6f7687
himanshu <himanshugaur2852001@gmail.com>
2020-09-03 22:43:19 +0530

    new files added

---
+++ scope.py
@@ -0,0 +1,117 @@
+# Python Scope
+
+# A variable is only available from inside the region it is
+# created. This is called scope.
+
+# Local Scope
+
+# A variable created inside a function belongs to the local
+# scope of that function, and can only be used inside that
+# function.
+
+def myfunc():
+    x = 300
+    print(x)
+
+
+myfunc()
+
+# print(x)  this will raise an error
+
+
+# Function Inside Function
+
+# As explained in the example above, the variable x is not
+# available outside the function, but it is available for any
+# function inside the function:
+
+def myfunc1():
+    x = 300
+
+    def myinnerfunc():
+        print(x)
+    myinnerfunc()
+
+
+myfunc1()
+
+
+# Global Scope
+
+# A variable created in the main body of the Python code is
+# a global variable and belongs to the global scope.
+
+# Global variables are available from within any scope,
+# global and local.
+
+# Example
+
+x = 300
+
+
+def myfunc2():
+    print(x)
+
+
+myfunc2()
+
+print(x)
+
+
+# Naming Variables
+
+# If you operate with the same variable name inside and
+# outside of a function, Python will treat them as two
+# separate variables,one available in the global scope
+# (outside the function) and one available in the local
+# scope (inside the function):
+
+x = 300
+
+
+def myfunc3():
+    x = 200
+    print(x)
+
+
+myfunc3()
+
+print(x)
+
+
+# Global Keyword
+
+# If you need to create a global variable, but are stuck in
+# the local scope, you can use the global keyword.
+
+# The global keyword makes the variable global.
+
+def myfunc4():
+    global x
+    x = 300
+
+
+myfunc4()
+
+print(x)
+
+
+# Also, use the global keyword if you want to make a change
+# to a global variable inside a function.
+
+# Example
+
+# To change the value of a global variable inside a function,
+# refer to the variable by using the global keyword:
+
+x = 300
+
+
+def myfunc5():
+    global x
+    x = 200
+
+
+myfunc5()
+
+print(x)
