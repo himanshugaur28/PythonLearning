c583025df93131fe472e28a18befc95149d95e28
Himanshu Gaur <69579469+himanshugaur28@users.noreply.github.com>
2020-09-03 22:55:42 +0530

    Add files via upload

---
+++ module/math_functions.py
@@ -0,0 +1,73 @@
+# Python Math
+
+# Python has a set of built-in math functions, including an extensive math module,
+# that allows you to perform mathematical tasks on numbers.
+
+# Built-in Math Functions
+
+# The min() and max() functions can be used to find the lowest or highest value in an iterable:
+
+import math
+x = min(3, 6, 2)
+y = max(2, 5, 3)
+
+print(x)
+print(y)
+
+
+# The abs() function returns the absolute (positive) value of the specified number:
+
+x = abs(-7.25)
+print(x)
+
+
+# The pow(x, y) function returns the value of x to the power of y (xy).
+
+x = pow(2, 3)
+print(x)
+
+
+# The Math Module
+
+# Python has also a built-in module called math, which extends the list of mathematical functions.
+# To use it, you must import the math module:
+
+
+# When you have imported the math module, you can start using methods and constants of the module.
+# The math.sqrt() method for example, returns the square root of a number
+
+x = math.sqrt(64)
+print(x)
+
+
+# The math.ceil() method rounds a number upwards to its nearest integer,
+# and the math.floor() method rounds a number downwards to its nearest integer, and returns the result
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x)
+print(y)
+
+
+# The math.pi constant, returns the value of PI (3.14...):
+
+x = math.pi
+
+print(x)
+
+
+#  -------------------- Advance Math methods ---------------------
+
+# Python has a built-in module that you can use for mathematical tasks.
+# The math module has a set of methods and constants.
+
+'''
+
+
+============== will do it later :)  ================
+
+
+'''
+
+print("hello")
